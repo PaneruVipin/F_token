82a946a99f62312181718f6006db39cd8e5b6cf9
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-05-08 20:58:24 +0530

    miner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import axios from "axios";
+import { validateToken, generateToken } from "dynamic-token";
 function App() {
   const [count, setCount] = useState(0);
+  const handleClick = () => {
+    axios.get("http://localhost:3333/test", {
+      headers: {
+        token: generateToken(992872828982),
+      },
+    });
+  };
   return (
     <div className="App">
       hello world
-      <button
-        onClick={() =>
-          validateToken(generateToken(999999999999), 999999999999)
-        }
-      >
-        click this
-      </button>
+      <button onClick={handleClick}>click this</button>
     </div>
   );
 }
